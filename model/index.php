78f80f84cef6ca3336806597b0bc6630d2042042
RenaudBernard59 <renaud.bernard@p59production.com>
2017-10-19 17:01:22 +0200

    Ininializing the model

--- model/index.php
+++ model/index.php
@@ -1,9 +1,12 @@
+<?php
+include('../vue/header.php');
 
 
-
-
+for ($i=1 ; $i <= 5 ; $i++) {
+	include('../vue/oneProject.php');
+}
 
 
     
-
-
+include('../vue/newProject.php');
+include('../vue/footer.php');
